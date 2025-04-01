412e4a3d63a01e116c853fc2b722232659818296
efeaydin-bio <efe.aydin@live.com>
2025-04-01 11:56:32 +0200

    Update analyzerFunctions.py

--- analyzerFunctions.py
+++ analyzerFunctions.py
@@ -118,7 +118,7 @@ def geneAnalyzer(subChoice, res, gene_of_interest, cre_index):
     new_bed_file.insert(7, "thickEnd", 2000)
 
     new_bed_file = new_bed_file.sort_values(by=[new_bed_file.columns[0], new_bed_file.columns[1]])
-    new_bed_file.to_csv(os.path.join(TRACKS_DIR, "tempCodingGenes.bed"), index=False, sep="\t", header=True)
+    new_bed_file.to_csv(os.path.join(TRACKS_DIR, "tempCodingGenes.bed"), index=False, sep="\t", header=False)
     
     gene_tracks_path = os.path.join(TRACKS_DIR, "geneTracks.ini")
     temp_tracks_path = os.path.join(TRACKS_DIR, "temp_gene_tracks.ini")

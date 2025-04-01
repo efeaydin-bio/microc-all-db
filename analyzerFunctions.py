ff00252960977e664b4fdca46b8ee69e9827e278
efeaydin-bio <efe.aydin@live.com>
2025-04-01 12:14:34 +0200

    Update analyzerFunctions.py

--- analyzerFunctions.py
+++ analyzerFunctions.py
@@ -136,8 +136,16 @@ def geneAnalyzer(subChoice, res, gene_of_interest, cre_index):
         temp_tracks_file.write("color = red\n")
         temp_tracks_file.write("title = Enhancer Links\n")
         temp_tracks_file.write("height = 5\n\n")
-        #  Third: Append rest of the tracks
-        temp_tracks_file.writelines(lines[1:])
+        #  Genes
+        temp_tracks_file.write("[genes]\n")
+        temp_tracks_file.write("file = tracks/tempCodingGenes.bed\n")
+        temp_tracks_file.write("file_type = bed\n")
+        temp_tracks_file.write("color = red\n")
+        temp_tracks_file.write("height = 10\n")
+        temp_tracks_file.write("title = Genes\n")
+        temp_tracks_file.write("fontsize = 12\n")
+        temp_tracks_file.write("arrow_interval = 5\n")
+        temp_tracks_file.write("gene_rows = 10\n\n")
     temp_tracks_file.close()
 
     output_file = "output_genome_track.png"

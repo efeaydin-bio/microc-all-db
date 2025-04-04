2d66a0e4daf388006d58e78a2c550fb00ef847b6
efeaydin-bio <efe.aydin@live.com>
2025-04-04 17:19:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -111,14 +111,12 @@ def pdf_page():
     with open(REFERENCE_MANUAL_PATH, "rb") as pdf_file:
         base64_pdf = base64.b64encode(pdf_file.read()).decode("utf-8")
 
-    #pdf_display = f'''
-     #   <iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="900px" style="border: none;"></iframe>
-    #'''
+    pdf_display = f'''
+        <iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="900px" style="border: none;"></iframe>
+    '''
 
-    st.markdown('''
-    <iframe src="/static/reference_manual.pdf" width="100%" height="900px" style="border: none;"></iframe>
-    ''', unsafe_allow_html=True)
-    #st.markdown(pdf_display, unsafe_allow_html=True)
+
+    st.markdown(pdf_display, unsafe_allow_html=True)
     
 
     if st.button("Back to Info Page"):

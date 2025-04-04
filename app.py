b935a63a6b05e097484c49c4a823f5f7753b60e1
efeaydin-bio <efe.aydin@live.com>
2025-04-04 19:07:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -111,11 +111,10 @@ def pdf_page():
     with open(REFERENCE_MANUAL_PATH, "rb") as pdf_file:
         base64_pdf = base64.b64encode(pdf_file.read()).decode("utf-8")
 
-   # pdf_display = f'''
- #       <iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="900px" style="border: none;"></iframe>
-#    '''
+    pdf_display = f'''
+    <iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="900px" style="border: none;"></iframe>
+    '''
 
-    pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
 
     st.markdown(pdf_display, unsafe_allow_html=True)
     
